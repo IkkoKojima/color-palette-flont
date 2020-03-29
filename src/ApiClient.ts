e4a3c13219a54675b07e68b0167d05697eb076d1
i_kojima <ikko.kojima@septeni-original.co.jp>
2020-03-29 18:15:27 +0900

    loglocket

--- src/ApiClient.ts
+++ src/ApiClient.ts
@@ -4,8 +4,8 @@ import { Color } from './ColorPalette'
 export function detectColor(imgBase64: string): Promise<Color[]> {
   return axios({
     method: "POST",
-    // url: 'https://color-palette-brgqt5m77a-an.a.run.app/',
-    url: 'http://127.0.0.1:5000/',
+    url: 'https://color-palette-brgqt5m77a-an.a.run.app/',
+    // url: 'http://127.0.0.1:5000/',
     responseType: "json",
     data:{
       image64:imgBase64
